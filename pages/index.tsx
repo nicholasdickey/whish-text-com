7f9f7f148dd7809b073b26ed4a874ef0f6fc89b3
nicholas.dickey <nicholas.dickey@hudsonwilde.com>
2023-07-10 16:34:52 -0400

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -388,15 +388,15 @@ export default function Home({ from: startFrom, to: startTo, occasion: startOcca
     <>
       <Head>
         <title>Wish Text Composer</title>
-        <meta name="twitter:card" content="summary_large_image"></meta>
+        <meta name="twitter:card" content="summary_large_image"/>
         <meta name="twitter:site" content="@wishtext" />
-        <meta name="twitter:title" content="Wish Text Composer"></meta>
+        <meta name="twitter:title" content="Wish Text Composer"/>
         <meta name="twitter:description" content="Are you tired of struggling to find the right words and perfect gifts for various occasions? Look no further! With WISH-TEXT.COM, our free AI-powered Assistant is here to make your life easier.
-Whether it's birthdays, graduations, holidays, or moments of illness or loss, WISH-TEXT.COM provides personalized messages and thoughtful gift recommendations, all at absolutely no cost."></meta>
-        <meta name="twitter:image" content="https://ucarecdn.com/76039561-e7f3-499b-9894-ea06abef4dc4/wishtextad.png"></meta>
+Whether it's birthdays, graduations, holidays, or moments of illness or loss, WISH-TEXT.COM provides personalized messages and thoughtful gift recommendations, all at absolutely no cost."/>
+        <meta name="twitter:image" content="https://ucarecdn.com/68d2ab3d-f58d-4015-beaf-4f2f7737643b/wishtextad1.png"/>
         <meta name="title" content="Wish Text Composer" />
         <meta property="og:title" content="Wish Text Composer" />
-        <meta property="og:image" content="https://ucarecdn.com/76039561-e7f3-499b-9894-ea06abef4dc4/wishtextad.png" />
+        <meta property="og:image" content="https://ucarecdn.com/68d2ab3d-f58d-4015-beaf-4f2f7737643b/wishtextad1.png" />
         <meta name="description" content="Are you tired of struggling to find the right words and perfect gifts for various occasions? Look no further! With WISH-TEXT.COM, our free AI-powered Assistant is here to make your life easier.
 Whether it's birthdays, graduations, holidays, or moments of illness or loss, WISH-TEXT.COM provides personalized messages and thoughtful gift recommendations, all at absolutely no cost." />
         <meta property="og:description" content="Are you tired of struggling to find the right words and perfect gifts for various occasions? Look no further! With WISH-TEXT.COM, our free AI-powered Assistant is here to make your life easier.
