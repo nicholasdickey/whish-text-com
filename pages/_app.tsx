3bfba00178d0c891033f7e08b02e02edae6171e5
nicholas.dickey <nicholas.dickey@hudsonwilde.com>
2023-07-11 19:07:26 -0400

    Add image

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,7 +21,7 @@ export default function MyApp(props: AppProps) {
     <React.Fragment>
       <Head>
         <title>Wish Text Composer</title>
-        <link href="/w-t-logo.png" rel="icon" />
+        <link href="/https://ucarecdn.com/3150242f-569d-4a42-8efb-d4b82ca1c6bb/wishtextlogo.png" rel="icon" />
         <meta content="minimum-scale=1, initial-scale=1, width=device-width" name="viewport" />
       </Head>
       <ThemeProvider theme={darkTheme}>
